dc66739a3c0337bdddce772afc2f16c9da875161
Darren Forsythe <forsythed90@gmail.com>
2018-02-13 19:24:19 +0000

    Example of Lombok and Mapstruct, with Junit5

---
+++ src/main/java/com/darrenforsythe/lombokwithmapstruct/dto/NameDTO.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package com.darrenforsythe.lombokwithmapstruct.dto;
+
+import lombok.Data;
+
+/**
+ * @author darren
+ *
+ */
+@Data
+public class NameDTO {
+
+	private String first;
+	private String last;
+
+}
